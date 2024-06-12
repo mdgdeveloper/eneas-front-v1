0a47db84888654ef9f762fda1f253936d9afcbbb
mdgdeveloper <se.rgiomadrigal@gmail.com>
2024-06-12 21:49:09 +0200

    main-page: update main page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-     
+      {/* Logo Block */}
+      <div>
+        Master Technical Certifications with AI-Powered Learning
+      </div>
+      <div>
+        Unlock your potential with AI-powered learning. Personalized Training and Progress Tracking for Technical mastery.
+      </div>
+      {/* Buttons */}
+
     </main>
   );
 }
